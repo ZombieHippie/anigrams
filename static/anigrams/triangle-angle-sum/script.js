4a493cf91485afd0275bb147c38b51591c655171
Cole R Lawrence <cole@zombiehippie.com>
2015-01-24 10:55:10 -0600

    Update library

--- static/anigrams/triangle-angle-sum/script.js
+++ static/anigrams/triangle-angle-sum/script.js
@@ -1,7 +1,7 @@
 // triangle-side-sum
 
 var points = []
-var new_change = function (x, y, point_no) = {
+var new_change = function (x, y, point_no) {
   points[point_no] = [x, y]
   createPoint(x, y, function (new_x, new_y, dx, dy) {
 
